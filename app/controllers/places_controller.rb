e11a3ee5c4e7957e6205373a4ce0ee58314cd18a
Tia Stutson <tmstutson@gmail.com>
2019-09-18 05:13:39 +0000

    Here vlidation was added to the submission forms to limit bogus content added to our cite. also updated alert messages css.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,8 +11,12 @@ end
 	end
 
 	def create 
-		current_user.places.create(place_params)
-		redirect_to root_path
+		@place = current_user.places.create(place_params)
+  		if @place.valid?
+    		redirect_to root_path
+  		else
+    		render :new, status: :unprocessable_entity
+  		end
 	end
 
 	def show
@@ -29,9 +33,19 @@ end
 
 	def update
 		@place = Place.find(params[:id])
+
 		if @place.user != current_user
 			return render plain: 'Not Allowed', status: :forbidden
 	end 
+
+	@place.update_attributes(place_params)
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :edit, status :unprocessable_entity
+		end
+	end
+	
 
 	def destroy
 		@place = Place.find(params[:id])
