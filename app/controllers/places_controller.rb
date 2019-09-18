d77f69abc1b73ef68f10bb7917807febdfa50dca
Tia Stutson <tmstutson@gmail.com>
2019-09-18 04:23:32 +0000

    Here we created forms that only allow the logged in user to edit, update and destroy our places.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+end
 
 	def index
 		@places = Place.all
@@ -20,12 +21,16 @@ class PlacesController < ApplicationController
 
 	def edit
 		@place = Place.find(params[:id])
+
+		if @place.user !=current_user
+			return render plain: 'Not Allowed',status: :forbidden
 	end
+end
 
 	def update
 		@place = Place.find(params[:id])
-		@place.update_attributes(place_params)
-		redirect_to root_path
+		if @place.user != current_user
+			return render plain: 'Not Allowed', status: :forbidden
 	end 
 
 	def destroy
