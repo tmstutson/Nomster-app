0a37578d305d96ff87fce99059028dc3af74a420
Tia Stutson <tmstutson@gmail.com>
2019-09-19 14:45:54 +0000

    here I renamed font.scss.css to font.css to debug an issue with heroku master pushing

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
- # This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_013309) do
+ActiveRecord::Schema.define(version: 2019_09_19_052510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2019_09_17_013309) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
