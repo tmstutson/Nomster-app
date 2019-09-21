38777976c3f36a28a1b53f19377a78f259f5ce1a
Tia Stutson <tmstutson@gmail.com>
2019-09-21 18:55:53 +0000

    heroku test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'nomster-tia-stutson.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
- 
+  # Code is not reloaded between requests. 
 
   config.cache_classes = true
 
