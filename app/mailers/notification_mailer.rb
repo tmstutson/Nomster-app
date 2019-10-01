3e59ce1d2c3b78fbf1cc20c3af06846f0f839e6a
Tia Stutson <tmstutson@gmail.com>
2019-10-01 01:02:43 +0000

    most recent nomster email configurations, adding a personal subject line and personalizing the outputs

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,10 @@ class NotificationMailer < ApplicationMailer
 	default from: "no-reply@nomsterapp.com"
 
 
-	def comment_added 
-		mail(to: "tmstutson@gmail.com", subject: "A comment has been added to your place")
+	def comment_added(comment)
+		@place = comment.place
+		@place_owner = @place.user
+		mail(to: @place_owner.email,
+			subject: "A comment has been added to your #{@place.name}")
   	end 
 end
