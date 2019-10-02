8b3551ff8970896c78ecf4acbb18f8ec788e1730
Tia Stutson <tmstutson@gmail.com>
2019-10-02 14:10:38 +0000

    email updates and sendgrid installation. Plus the most recent heroku deploy

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,18 +1,23 @@
 class Comment < ApplicationRecord
 	belongs_to :user
 	belongs_to :place
+	after_create :send_comment_email
 
-RATINGS = {
-	'one star': '1_star', 
-	'two stars': '2_star', 
-	'three stars': '3_stars', 
-	'four stars': '4_stars',
-	'five stars': '5_stars'
-}
+	RATINGS = {
+		'one star': '1_star', 
+		'two stars': '2_star', 
+		'three stars': '3_stars', 
+		'four stars': '4_stars',
+		'five stars': '5_stars'
+	}
 
 
-def humanized_rating
-	RATINGS.invert[self.rating]
-end
+	def humanized_rating
+		RATINGS.invert[self.rating]
+	end
+
+	def send_comment_email
+		NotificationMailer.comment_added(self).deliver_now
+	end
 end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,15 @@
 Rails.application.configure do
+   ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
+
+
   config.action_mailer.default_url_options = { host: 'nomster-tia-stutson.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -94,3 +105,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+
+
+
+
+
+
+
+
+
+
+
