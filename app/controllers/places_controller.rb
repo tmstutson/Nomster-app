dbde9526ebcbdd5e1eaf4d08f26703753d535ed2
Tia Stutson <tmstutson@gmail.com>
2019-12-09 20:24:17 +0000

    test .gitignore

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -27,7 +27,7 @@ class PlacesController < ApplicationController
 	def edit
 		@place = Place.find(params[:id])
 
-		if @place.user !=current_user
+		if @place.user != current_user
 			return render plain: 'Not Allowed',status: :forbidden
 		end
 	end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,6 @@
 class Photo < ActiveRecord::Base
-  mount_uploader :picture, PictureUploader
+	belongs_to :place
+	belongs_to :user 
+
+	mount_uploader :picture, PictureUploader
 end
