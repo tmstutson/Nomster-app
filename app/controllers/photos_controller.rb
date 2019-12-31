e65ef7cf3304b0c83b92b2e5c060d41216f5080f
Tia Stutson <tmstutson@gmail.com>
2019-12-31 01:06:41 +0000

    newest nomster code

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,12 +1,13 @@
 class PhotosController < ApplicationController
+	before_action :authenticate_user!
 
 	def create
 	  @place = Place.find(params[:place_id])
 	  @place.photos.create(photo_params.merge(user_id: current_user))
-	  	redirect_to place_path@place
+	  	redirect_to place_path(@place)
 	end
 
-private
+	private
 
 	def photo_params
 		params.require(:photo).permit(:picture, :caption)
